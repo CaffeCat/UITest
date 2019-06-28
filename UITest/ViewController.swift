9324cb5656ff96a3f57120f705a47d15ff665847
caffecat <weibin_caffe@outlook.com>
2019-06-28 14:24:17 +0800

    Initial Commit

---
+++ UITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITest
+//
+//  Created by caffe on 2019/6/28.
+//  Copyright © 2019 caffe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
